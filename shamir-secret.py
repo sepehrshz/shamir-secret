fe7fcff3ada9486d08434494f15fc3318a6a2d6c
sepehr1382 <sepehrshirazi27@gmail.com>
2024-07-02 21:48:17 +0330

    define lagrange function

--- shamir-secret.py
+++ shamir-secret.py
@@ -14,3 +14,16 @@ S_value = 2797
 t_threshold = 8
 n_shares = 7
 p_prime = 8797
+
+shares_generated = shares_generator(S_value, t_threshold, n_shares, p_prime)
+print("Shares:", shares_generated)
+
+def lagrange(x, y, p_val):
+        secret = 0
+        for i in range(len(x)):
+            sum = 1
+            for j in range(len(x)):
+                if i != j:
+                    sum *= (x[j]) * pow(x[j] - x[i], -1, p_val)
+            secret += y[i] * sum
+        return secret % p_val
